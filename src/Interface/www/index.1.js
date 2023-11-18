ec8192c54aa454bdb82385fc425d5ffd098b2383
allnulled <todosanulados@gmail.com>
2023-11-18 10:13:50 +0100

    detalles en interfaz

--- src/Interface/www/index.1.js
+++ src/Interface/www/index.1.js
@@ -1,12 +1,15 @@
 
-// [castelog:html5izable] ACTIVADO con: {"autor":"allnulled","nombre":"index","version":"1","contenido":{"head":"<head>\n    <title>🌐 Interfaz de usuario</title>\n    <meta charset=\"utf8\" />\n    <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\" />\n    <link rel=\"stylesheet\" type=\"text/css\" href=\"lib/win7/win7.scoped.2.css\" />\n    <link rel=\"stylesheet\" type=\"text/css\" href=\"lib/simple-grid/simple-grid.css\" />\n    <script src=\"lib/calo/calo.js\"></script>\n</head>","body":"<body><div id=\"app\"></div></body>"}}
+// [castelog:html5izable] ACTIVADO con: {"autor":"allnulled","nombre":"index","version":"1","contenido":{"head":"<head>\n    <title>🌐 Interfaz de usuario</title>\n    <meta charset=\"utf8\" />\n    <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\" />\n    <link rel=\"stylesheet\" type=\"text/css\" href=\"lib/win7/win7.scoped.2.css\" />\n    <script src=\"lib/calo/calo.js\"></script>\n    <style>\n      h5 {\n        background-color: #333;\n        color: white;\n        padding: 12px;\n        margin: 0px;\n        font-size: 12px;\n      }\n    </style>\n</head>","body":"<body><div id=\"app\"></div></body>"}}
 
 window.PaginaDeInicio = Castelog.metodos.un_componente_vue2("PaginaDeInicio",
   "<div class=\"PaginaDeInicio Component\">"
  + "    <h5>Interfaz de usuario</h5>"
- + "    <button>Botón</button>"
  + "  </div>",
-  function(component) {return { data() {try {
+  function(component) {return { props:{ root:{ type:Object,
+required:true
+}
+},
+data() {try {
 return { 
 };
 } catch(error) {
@@ -19,9 +22,27 @@ methods:{
 },
 watch:{ 
 },
+computed:{ 
+},
+beforeCreate() {
+},
+created() {
+},
 beforeMount() {
 },
 mounted() {
+},
+beforeUpdate() {
+},
+updated() {
+},
+beforeUnmount() {
+},
+unmounted() {
+},
+activated() {
+},
+deactivated() {
 }
 };},
   null);
@@ -31,9 +52,7 @@ window.App = Castelog.metodos.una_aplicacion_vue2(
  + "    <router-view :root=\"this\"></router-view>"
  + "  </div>",
   function(component) {return { data() {try {
-return { esquema:undefined,
-token_de_sesion:undefined,
-autentificacion:undefined
+return { 
 };
 } catch(error) {
 console.log(error);
